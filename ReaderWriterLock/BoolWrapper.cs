09b5530d60d3179f7bed66f5171dc6cdf7c99511
TerribleVoice <terriblevoice@ya.ru>
2021-04-23 23:24:43 +0500

    solved

---
+++ ReaderWriterLock/BoolWrapper.cs
@@ -0,0 +1,16 @@
+﻿namespace ReaderWriterLock
+{
+    public class BoolWrapper
+    {
+        public bool Value;
+
+        public BoolWrapper(bool value)
+        {
+            Value = value;
+        }
+        
+        public static BoolWrapper True = new BoolWrapper(true);
+        public static BoolWrapper False = new BoolWrapper(false);
+
+    }
+}
---
+++ ReaderWriterLock/RwLock.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+
+namespace ReaderWriterLock
+{
+    public class RwLock : IRwLock
+    {
+        private BoolWrapper isWriting = BoolWrapper.False;
+        private object locker;
+
+        public void ReadLocked(Action action)
+        {
+            while (Interlocked.CompareExchange(ref isWriting, BoolWrapper.False, BoolWrapper.False) == BoolWrapper.False)
+            {
+                action();
+                return;
+            }
+        }
+
+        public void WriteLocked(Action action)
+        {
+            while (Interlocked.CompareExchange(ref isWriting, BoolWrapper.True, BoolWrapper.False) == BoolWrapper.False)
+            {
+                action();
+                return;
+            }
+        }
+    }
+}
